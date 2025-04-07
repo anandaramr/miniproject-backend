a1d3322fcd0d29f464e77a88394cf4de76eac07e
Anandaram R <anandaramr1915@gmail.com>
2025-04-08 04:01:33 +0530

    Create echo server

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,16 @@ import cors from 'cors'
 import proxy from './routes/proxy'
 import auth from './routes/auth'
 import projects from './routes/projects'
+import echo from './routes/echo'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.use('/proxy', proxy)
+app.use('/echo', echo)
 app.use('/auth', auth)
 app.use('/projects', projects)
-
-app.post('/', (req, res) => {
-    console.log('req.body')
-    res.status(200).json(req.body)
-})
 
 app.listen(3000, () => {
     console.log("Listening at 3000")
